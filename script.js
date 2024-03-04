0963b90e416c10d5f0b16fa76cb429c27e958774
cereia <104402370+cereia@users.noreply.github.com>
2024-03-04 14:06:03 -0500

    Add display and update display logic
    Buttons were nonfunctional, this adds functionality to numbers and clear

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //when user clicks a number button, an operator, and another number
 //do that mathematical operation on those 2 numbers
-
 const operations = {
   '+': (n1, n2) => n1 + n2,
   '-': (n1, n2) => n1 - n2,
@@ -23,3 +22,27 @@ function operate(n1, op, n2) {
   //looks through operations obj for the correct operator key, then performs the operation
   return operations[operator](num1, num2);
 }
+
+//check that num1, operator, and num2 are available outside operate
+function operateCheck() {
+  console.log(num1, operator, num2); 
+}
+
+
+
+//calculator display 
+const display = document.querySelector('.display');
+display.textContent = 0;
+
+const numbers = document.querySelectorAll('.number');
+
+//if display shows 0, clicking any number except 0 replaces 0 
+//if you keep clicking, the clicked number gets added onto the end of what's already there
+numbers.forEach(num => num.addEventListener('click', () => {
+  return display.textContent == 0 
+  ? display.textContent = num.textContent 
+  : display.textContent += num.textContent;
+}));
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => display.textContent = 0);
