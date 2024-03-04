ed32c82ce4e592499d19b25fdcc6ceab1b5085b9
cereia <104402370+cereia@users.noreply.github.com>
2024-03-04 13:17:10 -0500

    Refactor operations into obj and func to perform
    Makes the code shorter and easier to read

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 //when user clicks a number button, an operator, and another number
 //do that mathematical operation on those 2 numbers
 
-//four basic operations of math
-function add(n1, n2) {
-  return n1 + n2;
-}
-
-function subtract(n1, n2) {
-  return n1 - n2;
-}
-
-function multiply(n1, n2) {
-  return n1 * n2;
-}
-
-function divide(n1, n2) {
-  //can't divide by 0, so set a special message to show if user is dividing by 0
-  if (Number(n2) === 0) {
-    return "Div by 0?!"
+const operations = {
+  '+': (n1, n2) => n1 + n2,
+  '-': (n1, n2) => n1 - n2,
+  '*': (n1, n2) => n1 * n2,
+  '/': (n1, n2) => {
+    if(Number(n2) === 0) {
+      return 'Div by 0?!'
+    }
+    return n1 / n2;
   }
-  return n1 / n2;
+}
+
+//create variables for the 3 user inputs
+let num1;
+let operator;
+let num2; 
+
+//takes 3 variables, 2 nums and an operation 
+//uses one of the above function based on the operation to perform the operation
+function operate(n1, op, n2) {
+  num1 = n1;
+  operator = op;
+  num2 = n2;
+  console.log(num1, operator, num2)
+
+  return operations[operator](num1, num2);
 }
