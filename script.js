82de849cf283c6a224ef203b2d3c57e3bf397d1e
cereia <104402370+cereia@users.noreply.github.com>
2024-03-04 15:25:46 -0500

    Add keydown event listeners on document object
    Added so user isn't limited to buttons and can use keyboard for inputs

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //when user clicks a number button, an operator, and another number
 //do that mathematical operation on those 2 numbers
+
+//CALCULATOR FUNCTIONALITY
 const operations = {
   '+': (n1, n2) => n1 + n2,
   '-': (n1, n2) => n1 - n2,
@@ -30,12 +32,13 @@ function operateCheck() {
 
 
 
-//calculator display 
+//CALCULATOR DISPLAY 
 const display = document.querySelector('.display');
 display.textContent = 0;
 
 const numbers = document.querySelectorAll('.number');
 
+//EVENT LISTENERS
 //if display shows 0, clicking any number except 0 replaces 0 
 //if you keep clicking, the clicked number gets added onto the end of what's already there
 numbers.forEach(num => num.addEventListener('click', () => {
@@ -44,5 +47,35 @@ numbers.forEach(num => num.addEventListener('click', () => {
   : display.textContent += num.textContent;
 }));
 
+document.addEventListener('keydown', (e) => {
+  if(e.key.match(/\d/)) {
+    return display.textContent == 0 
+    ? display.textContent = e.key 
+    : display.textContent += e.key;
+  }
+})
+
+//remove what's in display and resets it to 0
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', () => display.textContent = 0);
+
+document.addEventListener('keydown', (e) => {
+  if(e.key == 'Backspace') display.textContent = 0;
+})
+
+//clicking on any of the operations saves what's in the display to num1
+//and saves the operation to operator variable
+//clears display to prepare for next number; 
+    //don't actually remove the previous number until a new number is clicked, 
+    //then set new textContent
+//when = what's in display is saved to num2
+//operate function is called with num1, operator, and num2 
+//display is updated with calculated value
+
+//if an operator was clicked instead of =, 
+//what's in display is saved to num2
+//operate is called
+//display is updated 
+//what's in display is passed/saved as num1 
+//operation button pressed is saved as operator
+//prepare for num2 by clearing textContent
