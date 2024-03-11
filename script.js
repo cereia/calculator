0a750a6216d5582b358ccd340fde686a08cc655a
cereia <104402370+cereia@users.noreply.github.com>
2024-03-11 17:38:29 -0400

    Refactor to make code more DRY
    Keydown and click now pass to a function so there's no duplication code

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-//when user clicks a number button, an operator, and another number
-//do that mathematical operation on those 2 numbers
-
 //CALCULATOR FUNCTIONALITY
-const calculate = {
+const operations = {
   '+': (n1, n2) => n1 + n2,
   '-': (n1, n2) => n1 - n2,
   '*': (n1, n2) => n1 * n2,
@@ -14,15 +11,13 @@ let num1 = 0;
 let operator = '';
 let num2 = 0; 
 
-//takes 3 variables, 2 nums and an operation 
-//uses one of the above function based on the operation to perform the operation
 function operate(n1, op, n2) {
   num1 = Number(n1);
   operator = op;
   num2 = Number(n2);
 
-  //looks through calculate obj for the correct operator key, then performs the operation
-  return calculate[operator](num1, num2);
+  //looks through operations obj for the correct operator key, then performs the operation
+  return operations[operator](num1, num2);
 }
 
 
@@ -34,8 +29,9 @@ const numbers = document.querySelectorAll('.number');
 const clear = document.querySelector('.clear');
 
 //EVENT LISTENERS
+
 //EVENT LISTENER HELPER FUNCTIONS
-//remove selected class from operation buttons
+//clear display for next number by remove selected class from operation button
 function removeSelected() {
   //turn a node list into a proper array to iterate
   let opBtnsArr = [...opBtns];
@@ -48,7 +44,7 @@ function removeSelected() {
   }
 }
 
-//helper function to clear user inputs
+//clear user inputs
 function clearInputs() {
   num1 = 0;
   operator = '';
@@ -59,6 +55,7 @@ function clearInputs() {
 //if display shows 0, clicking any number except 0 replaces 0 
 //if it's not 0, the clicked number gets added onto the end of what's already there
 numbers.forEach(num => num.addEventListener('click', () => {
+  //call removeSelected first to make display 0 before moving to ternary
   removeSelected();
 
   return display.textContent == 0 
@@ -91,28 +88,15 @@ document.addEventListener('keydown', (e) => {
   };  
 });
 
-//add event listener to operator buttons, including =
-opBtns.forEach(btn => btn.addEventListener('click', () => {
+//click listener to operator buttons and =
+opBtns.forEach(btn => btn.addEventListener('click', (e) => {
   //add selected class that helps with clearing display
   btn.classList.add('selected');
-  //if operator is undefined, save what's in display to num1 and button's content to operator
-  if(btn.textContent !== '=' && !operator) {
-    num1 = display.textContent;
-    operator = btn.textContent;
-  } else {
-    num2 = display.textContent;
-    display.textContent = operate(num1, operator, num2);
-    clearInputs();
-
-    //if the button clicked isn't =, save what's in display to num1 and button to operator
-    if(btn.textContent !== '=') {
-      num1 = display.textContent;
-      operator = btn.textContent;
-    }
-  }
+  //pass button's symbol to handler
+  calculateHandler(e.target.textContent);
 }));
 
-//keyboard support for calculate
+//keyboard listener for operations
 document.addEventListener('keydown', (e) => {
   //add selected class to help with clearing display
   let opBtnsArr = [...opBtns];
@@ -123,21 +107,27 @@ document.addEventListener('keydown', (e) => {
     }
   }
 
-  let keys = Object.keys(calculate);
-  if(keys.includes(e.key) || e.key === '=' || e.key === 'Enter') {
-    if(!operator && keys.includes(e.key)) {
-      num1 = display.textContent;
-      operator = e.key;
-    } else {
-      num2 = display.textContent;
-      display.textContent = operate(num1, operator, num2);
-      
-      clearInputs();
-      //if e.key is not = or enter, save display to num1 and e.key to operator
-      if(keys.includes(e.key)) {
-        num1 = display.textContent;
-        operator = e.key;
-        }
-    }
+  let keys = Object.keys(operations);
+  if(keys.includes(e.key) || e.key === '=') {
+    calculateHandler(e.key);
+  } else if (e.key === 'Enter') {
+    calculateHandler('=');
   }
 })
+
+//click and keydown handler for operations
+function calculateHandler(symbol) {
+  if(symbol !== '=' && !operator) {
+    num1 = display.textContent;
+    operator = symbol;
+  } else {
+    num2 = display.textContent;
+    display.textContent = operate(num1, operator, num2);
+    clearInputs();
+
+    if(symbol !== '=') {
+      num1 = display.textContent;
+      operator = symbol;
+    }
+  }
+}
